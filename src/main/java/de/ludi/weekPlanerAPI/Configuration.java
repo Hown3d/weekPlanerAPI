ee2b14a151aaa18c71d60f0ed4632207201eb271
stluhoeh <lukas.hoehl@stud.h-da.de>
2020-07-24 13:04:52 +0200

    Encrypted Password

--- src/main/java/de/ludi/weekPlanerAPI/Configuration.java
+++ src/main/java/de/ludi/weekPlanerAPI/Configuration.java
@@ -1,10 +1,13 @@
 package de.ludi.weekPlanerAPI;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import java.util.Locale;
 import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@EnableEncryptableProperties
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
--- src/main/java/de/ludi/weekPlanerAPI/WeekPlanerApiApplication.java
+++ src/main/java/de/ludi/weekPlanerAPI/WeekPlanerApiApplication.java
@@ -1,5 +1,6 @@
 package de.ludi.weekPlanerAPI;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
